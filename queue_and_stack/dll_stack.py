b7716679e6bc680d90759cd07f9e9cf3500bcc2c
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-14 18:02:59 +0100

    complete mvp

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -7,12 +7,27 @@ class Stack:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        # pass
+        # remove item from stack
+        if self.size > 0 and self.size != 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+        else:
+            return None
+
 
     def len(self):
-        pass
+        # pass
+        # get number of items in stack
+        if self.size > 0:
+            return self.size
+        else:
+            return 0
